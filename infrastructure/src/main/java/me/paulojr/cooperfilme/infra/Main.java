e461e0bfdbbbd01e77e67751eb1a365a7994150b
Paulo Jr <pauloricardo.jr1@gmail.com>
2024-09-16 01:46:12 -0300

    feat: removido encode de senha

--- infrastructure/src/main/java/me/paulojr/cooperfilme/infra/Main.java
+++ infrastructure/src/main/java/me/paulojr/cooperfilme/infra/Main.java
@@ -3,22 +3,13 @@ package me.paulojr.cooperfilme.infra;
 import me.paulojr.cooperfilme.infra.config.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@Component
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(WebServerConfig.class, args);
     }
 
-
-
-    public Main(PasswordEncoder passwordEncoder) {
-        System.out.println(passwordEncoder.encode("cooperfilmes"));
-
-    }
 
 }
